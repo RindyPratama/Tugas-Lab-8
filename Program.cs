ccb28e002d3e0d7b4f5353b22a3f86d2fee4d310
RindyPratama <61863702+RindyPratama@users.noreply.github.com>
2020-06-22 20:39:55 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasLab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Tugas Lab 8 (Pertemuan 11) - Polymorphsim, Inheritance, Abstraction & Collection";
+
+            KaryawanTetap karyawanTetap = new KaryawanTetap();
+            karyawanTetap.Nik = "111-222-333";
+            karyawanTetap.Nama = "Elga Eleazaar";
+            karyawanTetap.GajiBulanan = 4000000;
+
+            KaryawanHarian karyawanHarian = new KaryawanHarian();
+            karyawanHarian.Nik = "444-555-666";
+            karyawanHarian.Nama = "Maryadi";
+            karyawanHarian.JumlahJamKerja = 8;
+            karyawanHarian.UpahPerJam = 13000;
+
+            Sales sales = new Sales();
+            sales.Nik = "999-888-777";
+            sales.Nama = "Andrean";
+            sales.JumlahPenjualan = 30;
+            sales.Komisi = 45000;
+
+            List<Karyawan> listKaryawan = new List<Karyawan>();
+
+            listKaryawan.Add(karyawanTetap);
+            listKaryawan.Add(karyawanHarian);
+            listKaryawan.Add(sales);
+
+            int noUrut = 1;
+            foreach (Karyawan karyawan in listKaryawan)
+            {
+                Console.WriteLine("{0}. Nik: {1}, Nama: {2}, Gaji: {3:N0}", noUrut, karyawan.Nik, karyawan.Nama, karyawan.Gaji());
+                noUrut++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
